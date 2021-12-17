abc6e1231fceecad12d7809004c65184def62c61
roseckyj <rosecky.jonas@centrum.cz>
2021-12-17 14:16:27 +0100

    Before version update

--- src/components/MapWrapper.tsx
+++ src/components/MapWrapper.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import { CircleMarker, Map, Polyline, TileLayer, ZoomControl } from 'react-leaflet';
+import { CircleMarker, Map, Polyline, Popup, TileLayer, ZoomControl } from 'react-leaflet';
+import { shapes } from '../shapes';
 import { getDistance } from '../utils/getDistance';
 import { Point } from '../utils/Point';
 
-const THRESHOLD = 5;
+const THRESHOLD = 10;
+const USE_MOUSE = true;
 
 type IMapWrapperProps = {};
 
 type IMapWrapperState = {
     center: Point;
     zoom: number;
-    location: Point;
+    location: Point | null;
     points: Point[];
     pointId: number;
 };
 
 export default class MapWrapper extends React.Component<IMapWrapperProps, IMapWrapperState> {
     state = {
-        center: [49.5609117, 15.9407] as Point,
+        center: this.shape[0],
         zoom: 17,
-        location: [49.5609117, 15.9407] as Point,
+        location: null,
         points: [] as Point[],
         pointId: 0,
     };
@@ -44,46 +46,28 @@ export default class MapWrapper extends React.Component<IMapWrapperProps, IMapWr
         this.setState(toSet);
     }
 
-    shape: Point[] = [
-        [49.560361, 15.941539],
-        [49.560353, 15.941695],
-        [49.560309, 15.941829],
-        [49.560183, 15.941898],
-        [49.5601, 15.941834],
-        [49.560068, 15.941652],
-        [49.560134, 15.941491],
-        [49.560235, 15.941448],
-        [49.560312, 15.941491],
-        [49.56034, 15.941491],
-        [49.560058, 15.941169],
-        [49.55994, 15.941163],
-        [49.559838, 15.941292],
-        [49.559863, 15.941512],
-        [49.559943, 15.941593],
-        [49.560037, 15.941587],
-        [49.560093, 15.941507],
-        [49.56011, 15.941378],
-        [49.560089, 15.94126],
-        [49.559916, 15.941652],
-        [49.559856, 15.94178],
-        [49.559797, 15.941925],
-        [49.559727, 15.942059],
-        [49.559672, 15.942199],
-        [49.559616, 15.94229],
-        [49.559568, 15.94236],
-        [49.559557, 15.942488],
-        [49.559616, 15.942596],
-        [49.559706, 15.942606],
-        [49.559741, 15.942542],
-        [49.559769, 15.942446],
-        [49.559787, 15.942338],
-        [49.559682, 15.942242],
-        [49.559814, 15.942301],
-        [49.559902, 15.942129],
-        [49.559943, 15.941995],
-        [49.560002, 15.941898],
-        [49.560058, 15.941727],
-    ];
+    get shape() {
+        return shapes.snowman;
+    }
+
+    requestLocation = () => {
+        if (!navigator.geolocation) {
+            console.error('Geolocation is not supported by your browser');
+        } else {
+            navigator.geolocation.getCurrentPosition(this.gotLocation, () => {
+                console.error('Location error!');
+            });
+        }
+    };
+
+    gotLocation = (loc: GeolocationPosition) => {
+        if (!USE_MOUSE) this.positionChanged([loc.coords.latitude, loc.coords.longitude]);
+        this.requestLocation();
+    };
+
+    componentDidMount() {
+        this.requestLocation();
+    }
 
     render() {
         return (
@@ -91,7 +75,7 @@ export default class MapWrapper extends React.Component<IMapWrapperProps, IMapWr
                 viewport={{ center: this.state.center, zoom: this.state.zoom }}
                 zoomControl={false}
                 onmousemove={(e) => {
-                    this.positionChanged([e.latlng.lat, e.latlng.lng]);
+                    if (USE_MOUSE) this.positionChanged([e.latlng.lat, e.latlng.lng]);
                 }}
                 onViewportChange={(e) => {
                     this.setState({ center: e.center!, zoom: e.zoom! });
@@ -100,22 +84,38 @@ export default class MapWrapper extends React.Component<IMapWrapperProps, IMapWr
             >
                 <ZoomControl position="topright" />
                 <TileLayer url="https://mapserver.mapy.cz/ophoto-m/{z}-{x}-{y}" opacity={0.3} />
-                <Polyline color="black" positions={this.state.points} />
-                <CircleMarker
-                    center={this.state.location}
-                    radius={10}
-                    color="transparent"
-                    fillColor="#7579EE"
-                    fillOpacity={0.5}
-                ></CircleMarker>
-                {this.state.pointId < this.shape.length && (
+                <Polyline color="black" positions={this.shape.filter((val, i) => i <= this.state.pointId)} />
+                <Polyline color="#7579EE" positions={this.state.points} opacity={0.2} weight={30} />
+                {this.state.location && (
                     <CircleMarker
-                        center={this.shape[this.state.pointId]}
-                        radius={10}
+                        center={this.state.location!}
+                        radius={15}
                         color="transparent"
-                        fillColor="black"
+                        fillColor="#7579EE"
                         fillOpacity={0.5}
                     ></CircleMarker>
+                )}
+                {this.state.pointId === 0 && (
+                    <CircleMarker
+                        center={this.shape[this.state.pointId]}
+                        radius={15}
+                        color="transparent"
+                        fillColor="black"
+                        fillOpacity={0.8}
+                    ></CircleMarker>
+                )}
+                {(this.state.pointId === this.shape.length || true) && (
+                    <CircleMarker
+                        center={this.shape[this.shape.length - 1]}
+                        radius={15}
+                        color="transparent"
+                        fillOpacity={0}
+                        isActive
+                    >
+                        <Popup closeOnEscapeKey={false} closeOnClick={false} autoClose={false} closeButton={false}>
+                            Už jsi blízko konce, až budeš chtít, můžeš vypnout kreslení...
+                        </Popup>
+                    </CircleMarker>
                 )}
 
                 <img
