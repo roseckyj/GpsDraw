a0f5d4d57842addb861969f0f161eb1ac4ebe602
roseckyj <rosecky.jonas@centrum.cz>
2021-12-17 14:56:48 +0100

    Added alerts

--- src/components/MapWrapper.tsx
+++ src/components/MapWrapper.tsx
@@ -15,6 +15,8 @@ type IMapWrapperState = {
     location: Point | null;
     points: Point[];
     pointId: number;
+    drawing: boolean;
+    wasCloseToStart: boolean;
 };
 
 export default class MapWrapper extends React.Component<IMapWrapperProps, IMapWrapperState> {
@@ -24,30 +26,32 @@ export default class MapWrapper extends React.Component<IMapWrapperProps, IMapWr
         location: null,
         points: [] as Point[],
         pointId: 0,
+        drawing: false,
+        wasCloseToStart: false,
     };
-
-    get drawEnabled() {
-        return this.state.pointId < this.shape.length && this.state.pointId > 0;
-    }
 
     positionChanged(position: Point) {
         let toSet: any = {};
-        if (this.drawEnabled) {
+        if (this.state.drawing) {
             this.state.points.push(position);
             toSet.points = [...this.state.points];
+
+            if (
+                this.state.pointId < this.shape.length &&
+                getDistance(position, this.shape[this.state.pointId]) < THRESHOLD
+            ) {
+                toSet.pointId = this.state.pointId + 1;
+            }
         }
-        if (
-            this.state.pointId < this.shape.length &&
-            getDistance(position, this.shape[this.state.pointId]) < THRESHOLD
-        ) {
-            toSet.pointId = this.state.pointId + 1;
+        if (!this.state.wasCloseToStart && getDistance(this.shape[0], position) <= THRESHOLD) {
+            toSet.wasCloseToStart = true;
         }
         toSet.location = position;
         this.setState(toSet);
     }
 
     get shape() {
-        return shapes.snowman;
+        return shapes.hvezda;
     }
 
     requestLocation = () => {
@@ -71,48 +75,75 @@ export default class MapWrapper extends React.Component<IMapWrapperProps, IMapWr
 
     render() {
         return (
-            <Map
-                viewport={{ center: this.state.center, zoom: this.state.zoom }}
-                zoomControl={false}
-                onmousemove={(e) => {
-                    if (USE_MOUSE) this.positionChanged([e.latlng.lat, e.latlng.lng]);
-                }}
-                onViewportChange={(e) => {
-                    this.setState({ center: e.center!, zoom: e.zoom! });
-                }}
-                maxZoom={20}
-            >
-                <ZoomControl position="topright" />
-                <TileLayer url="https://mapserver.mapy.cz/ophoto-m/{z}-{x}-{y}" opacity={0.3} />
-                <Polyline color="black" positions={this.shape.filter((val, i) => i <= this.state.pointId)} />
-                <Polyline color="#7579EE" positions={this.state.points} opacity={0.2} weight={30} />
-                {this.state.location && (
-                    <CircleMarker
-                        center={this.state.location!}
-                        radius={15}
-                        color="transparent"
-                        fillColor="#7579EE"
-                        fillOpacity={0.5}
-                    ></CircleMarker>
-                )}
-                {this.state.pointId === 0 && (
-                    <CircleMarker
-                        center={this.shape[this.state.pointId]}
-                        radius={15}
-                        color="transparent"
-                        fillColor="black"
-                        fillOpacity={0.8}
-                    ></CircleMarker>
-                )}
+            <>
+                <Map
+                    viewport={{ center: this.state.center, zoom: this.state.zoom }}
+                    zoomControl={false}
+                    onmousemove={(e) => {
+                        if (USE_MOUSE) this.positionChanged([e.latlng.lat, e.latlng.lng]);
+                    }}
+                    onViewportChange={(e) => {
+                        this.setState({ center: e.center!, zoom: e.zoom! });
+                    }}
+                    maxZoom={20}
+                >
+                    <ZoomControl position="topright" />
+                    <TileLayer url="https://mapserver.mapy.cz/ophoto-m/{z}-{x}-{y}" opacity={0.3} />
+                    <Polyline color="black" positions={this.shape.filter((val, i) => i <= this.state.pointId)} />
+                    <Polyline color="#7579EE" positions={this.state.points} opacity={0.2} weight={30} />
+                    {this.state.location && (
+                        <CircleMarker
+                            center={this.state.location!}
+                            radius={15}
+                            color="transparent"
+                            fillColor="#7579EE"
+                            fillOpacity={0.5}
+                        ></CircleMarker>
+                    )}
+                    {this.state.pointId === 0 && (
+                        <CircleMarker
+                            center={this.shape[this.state.pointId]}
+                            radius={15}
+                            color="transparent"
+                            fillColor="black"
+                            fillOpacity={0.8}
+                        ></CircleMarker>
+                    )}
 
-                <img
-                    src={process.env.PUBLIC_URL + '/assets/ostrovy-logo.png'}
-                    alt="Ostrovy pohody"
-                    className="logoOp"
-                />
-                <img src={process.env.PUBLIC_URL + '/assets/duha-logo.png'} alt="Duha AZ" className="logoDuha" />
-                <div className="notification"></div>
-            </Map>
+                    <img
+                        src={process.env.PUBLIC_URL + '/assets/ostrovy-logo.png'}
+                        alt="Ostrovy pohody"
+                        className="logoOp"
+                    />
+                    <img src={process.env.PUBLIC_URL + '/assets/duha-logo.png'} alt="Duha AZ" className="logoDuha" />
+                </Map>
+                <div className="notification">
+                    {this.state.drawing && this.state.pointId === this.shape.length && (
+                        <div className="alert alert-primary" role="alert">
+                            <p>Už jsi skoro u cíle! Až budeš chtít ukončit tvar, můžeš kliknout na tlačítko</p>
+                            <button
+                                type="button"
+                                className="btn btn-primary"
+                                onClick={() => this.setState({ drawing: false })}
+                            >
+                                Dokončit obrazec
+                            </button>
+                        </div>
+                    )}
+                    {!this.state.drawing && this.state.pointId === 0 && this.state.wasCloseToStart && (
+                        <div className="alert alert-primary" role="alert">
+                            <p>Vítej! Už jsi skoro na startu. Až budeš chtít začít, můžeš kliknout na tlačítko</p>
+                            <button
+                                type="button"
+                                className="btn btn-primary"
+                                onClick={() => this.setState({ drawing: true })}
+                            >
+                                Začít odkrývat
+                            </button>
+                        </div>
+                    )}
+                </div>
+            </>
         );
     }
 }
